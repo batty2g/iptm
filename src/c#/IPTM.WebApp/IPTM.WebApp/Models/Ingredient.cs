18ff28218c5ae1dcaec242f31123ee9b433b983a
james goerke <james.d.goerke@gmail.com>
2017-02-25 14:03:01 -0500

    Adding Base Models

---
+++ src/c#/IPTM.WebApp/IPTM.WebApp/Models/Ingredient.cs
@@ -0,0 +1,46 @@
+﻿namespace IPTM.WebApp.Models
+{
+    public class Ingredient
+    {
+        private int _id;
+        public int Id {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private int _name;
+        public int Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _description;
+        public int Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private int _ingredientTypeId;
+        public int IngredientTypeId
+        {
+            get { return _ingredientTypeId; }
+            private set { _ingredientTypeId = value; }
+        }
+
+        private IngredientType _ingredientType;
+        public IngredientType IngredientType
+        {
+            get { return _ingredientType; }
+            set { _ingredientType = value; }
+        }
+
+        private int _proof;
+        public int Proof
+        {
+            get { return _proof; }
+            set { _proof = value; }
+        }
+    }
+}
---
+++ src/c#/IPTM.WebApp/IPTM.WebApp/Models/Recipe.cs
@@ -0,0 +1,33 @@
+﻿namespace IPTM.WebApp.Models
+{
+    public class Recipe
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private int _name;
+        public int Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _description;
+        public int Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private int _instructions;
+        public int Instructions
+        {
+            get { return _instructions; }
+            set { _instructions = value; }
+        }
+    }
+}
